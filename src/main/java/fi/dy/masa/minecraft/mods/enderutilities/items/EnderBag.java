ff4e7d03b0af6cc384ef56d020ebbac97e0b1429
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2014-05-15 10:15:15 +0300

    Auto-commit @ 2014-05-15_10:15:15

---
+++ src/main/java/fi/dy/masa/minecraft/mods/enderutilities/items/EnderBag.java
@@ -0,0 +1,13 @@
+package fi.dy.masa.minecraft.mods.enderutilities.items;
+
+import net.minecraft.item.Item;
+
+public class EnderBag extends Item
+{
+	public EnderBag()
+	{
+		super();
+		this.maxStackSize = 1;
+		//this.setCreativeTab(CreativeTab.);
+	}
+}
