a8bd330a8197b242db4d1ca77f6a68ec20c902bd
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2014-05-17 15:45:04 +0300

    Auto-commit @ 2014-05-17_15:45:04

--- src/main/java/fi/dy/masa/minecraft/mods/enderutilities/block/EnderFurnace.java
+++ src/main/java/fi/dy/masa/minecraft/mods/enderutilities/block/EnderFurnace.java
@@ -43,6 +43,29 @@ public class EnderFurnace extends BlockContainer
 		return Item.getItemFromBlock(Blocks.furnace);
 	}
 
+	// Returns a new instance of a block's tile entity class. Called on placing the block.
+	public TileEntity createNewTileEntity(World world, int i)
+	{
+		return new TileEntityFurnace();
+	}
+
+	// Called when the block is placed in the world.
+	public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase entity, ItemStack stack)
+	{
+		int rot = MathHelper.floor_double((double)(entity.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
+		world.setBlockMetadataWithNotify(x, y, z, rot, 2);
+
+		//if (rot == 0) { world.setBlockMetadataWithNotify(x, y, z, 2, 2); }
+		//if (rot == 1) { world.setBlockMetadataWithNotify(x, y, z, 5, 2); }
+		//if (rot == 2) { world.setBlockMetadataWithNotify(x, y, z, 3, 2); }
+		//if (rot == 3) { world.setBlockMetadataWithNotify(x, y, z, 4, 2); }
+
+		if (stack.hasDisplayName())
+		{
+			((TileEntityFurnace)world.getTileEntity(x, y, z)).func_145951_a(stack.getDisplayName());
+		}
+	}
+
 	/**
 	 * Called whenever the block is added into the world. Args: world, x, y, z
 	 */
@@ -87,29 +110,8 @@ public class EnderFurnace extends BlockContainer
 			p_149930_1_.setBlockMetadataWithNotify(p_149930_2_, p_149930_3_, p_149930_4_, b0, 2);
 		}
 	}
-*/
-	/**
-	 * Gets the block's texture. Args: side, meta
-	 */
-	@SideOnly(Side.CLIENT)
-	public IIcon getIcon(int side, int meta)
-	{
-		return side == 1 ? this.iconTop : (side == 0 ? this.iconTop : (side != meta ? this.blockIcon : this.iconFront));
-	}
 
-	@SideOnly(Side.CLIENT)
-	public void registerBlockIcons(IIconRegister iconRegister)
-	{
-		this.blockIcon = iconRegister.registerIcon(Reference.NAME_ITEM_ENDER_FURNACE + "_side");
-		this.iconTop = iconRegister.registerIcon(Reference.NAME_ITEM_ENDER_FURNACE + "_top");
-		// FIXME how can we do the front icon based on state? Needs TESR?
-		this.iconFront = iconRegister.registerIcon(Reference.NAME_ITEM_ENDER_FURNACE + "_front_off");
-	}
-
-	/**
-	 * Called upon block activation (right click on the block.)
-	 */
-/*
+	// Called upon block activation (right click on the block.)
 	public boolean onBlockActivated(World p_149727_1_, int p_149727_2_, int p_149727_3_, int p_149727_4_, EntityPlayer p_149727_5_, int p_149727_6_, float p_149727_7_, float p_149727_8_, float p_149727_9_)
 	{
 		if (p_149727_1_.isRemote)
@@ -128,11 +130,8 @@ public class EnderFurnace extends BlockContainer
 			return true;
 		}
 	}
-*/
-	/**
-	 * Update which block the furnace is using depending on whether or not it is burning
-	 */
-/*
+
+	// Update which block the furnace is using depending on whether or not it is burning
 	public static void updateFurnaceBlockState(boolean p_149931_0_, World p_149931_1_, int p_149931_2_, int p_149931_3_, int p_149931_4_)
 	{
 		int l = p_149931_1_.getBlockMetadata(p_149931_2_, p_149931_3_, p_149931_4_);
@@ -157,51 +156,7 @@ public class EnderFurnace extends BlockContainer
 			p_149931_1_.setTileEntity(p_149931_2_, p_149931_3_, p_149931_4_, tileentity);
 		}
 	}
-*/
-	/**
-	 * Returns a new instance of a block's tile entity class. Called on placing the block.
-	 */
 
-	public TileEntity createNewTileEntity(World world, int i)
-	{
-		return new TileEntityFurnace();
-	}
-
-	/**
-	 * Called when the block is placed in the world.
-	 */
-/*
-	public void onBlockPlacedBy(World p_149689_1_, int p_149689_2_, int p_149689_3_, int p_149689_4_, EntityLivingBase p_149689_5_, ItemStack p_149689_6_)
-	{
-		int l = MathHelper.floor_double((double)(p_149689_5_.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
-
-		if (l == 0)
-		{
-			p_149689_1_.setBlockMetadataWithNotify(p_149689_2_, p_149689_3_, p_149689_4_, 2, 2);
-		}
-
-		if (l == 1)
-		{
-			p_149689_1_.setBlockMetadataWithNotify(p_149689_2_, p_149689_3_, p_149689_4_, 5, 2);
-		}
-
-		if (l == 2)
-		{
-			p_149689_1_.setBlockMetadataWithNotify(p_149689_2_, p_149689_3_, p_149689_4_, 3, 2);
-		}
-
-		if (l == 3)
-		{
-			p_149689_1_.setBlockMetadataWithNotify(p_149689_2_, p_149689_3_, p_149689_4_, 4, 2);
-		}
-
-		if (p_149689_6_.hasDisplayName())
-		{
-			((TileEntityFurnace)p_149689_1_.getTileEntity(p_149689_2_, p_149689_3_, p_149689_4_)).func_145951_a(p_149689_6_.getDisplayName());
-		}
-	}
-*/
-/*
 	public void breakBlock(World world, int x, int y, int z, Block block, int meta)
 	{
 		if (!field_149934_M)
@@ -252,11 +207,22 @@ public class EnderFurnace extends BlockContainer
 	
 		super.breakBlock(world, x, y, z, block, meta);
 	}
-*/
-	/**
-	 * A randomly called display update to be able to add particles or other items for display
-	 */
-/*
+
+	// If this returns true, then comparators facing away from this block will use the value from
+	// getComparatorInputOverride instead of the actual redstone signal strength.
+	public boolean hasComparatorInputOverride()
+	{
+		return true;
+	}
+
+	// If hasComparatorInputOverride returns true, the return value from this is used instead of the redstone signal
+	// strength when this block inputs to a comparator.
+	public int getComparatorInputOverride(World world, int x, int y, int z, int meta)
+	{
+		return Container.calcRedstoneFromInventory((IInventory)world.getTileEntity(x, y, z));
+	}
+
+	// A randomly called display update to be able to add particles or other items for display
 	@SideOnly(Side.CLIENT)
 	public void randomDisplayTick(World p_149734_1_, int p_149734_2_, int p_149734_3_, int p_149734_4_, Random p_149734_5_)
 	{
@@ -292,32 +258,28 @@ public class EnderFurnace extends BlockContainer
 		}
 	}
 */
-	/**
-	 * If this returns true, then comparators facing away from this block will use the value from
-	 * getComparatorInputOverride instead of the actual redstone signal strength.
-	 */
-/*
-	public boolean hasComparatorInputOverride()
-	{
-		return true;
-	}
-*/
-	/**
-	 * If hasComparatorInputOverride returns true, the return value from this is used instead of the redstone signal
-	 * strength when this block inputs to a comparator.
-	 */
-/*
-	public int getComparatorInputOverride(World world, int x, int y, int z, int meta)
-	{
-		return Container.calcRedstoneFromInventory((IInventory)world.getTileEntity(x, y, z));
-	}
-*/
-	/**
-	 * Gets an item for the block being called on. Args: world, x, y, z
-	 */
+	// Gets an item for the block being called on. Args: world, x, y, z
 	@SideOnly(Side.CLIENT)
 	public Item getItem(World world, int x, int y, int z)
 	{
-			return Item.getItemFromBlock(Blocks.furnace);
+		return Item.getItemFromBlock(Blocks.furnace);
+	}
+
+	// Gets the block's texture. Args: side, meta
+	@SideOnly(Side.CLIENT)
+	public IIcon getIcon(int side, int meta)
+	{
+		if (side == 0 || side == 1) { return this.iconTop; }
+		if (side != meta) { return this.blockIcon; }
+		return this.iconFront;
+	}
+
+	@SideOnly(Side.CLIENT)
+	public void registerBlockIcons(IIconRegister iconRegister)
+	{
+		this.blockIcon = iconRegister.registerIcon(Reference.NAME_ITEM_ENDER_FURNACE + "_side");
+		this.iconTop = iconRegister.registerIcon(Reference.NAME_ITEM_ENDER_FURNACE + "_top");
+		// FIXME how can we do the front icon based on state? Needs TESR?
+		this.iconFront = iconRegister.registerIcon(Reference.NAME_ITEM_ENDER_FURNACE + "_front_off");
 	}
 }
