f50f0767e8b8ab14f188a08072edce67166a7614
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2014-05-18 09:00:12 +0300

    Auto-commit @ 2014-05-18_09:00:12

--- src/main/java/fi/dy/masa/minecraft/mods/enderutilities/items/EnderBow.java
+++ src/main/java/fi/dy/masa/minecraft/mods/enderutilities/items/EnderBow.java
@@ -1,25 +1,17 @@
 package fi.dy.masa.minecraft.mods.enderutilities.items;
 
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
-import fi.dy.masa.minecraft.mods.enderutilities.creativetab.CreativeTab;
-import fi.dy.masa.minecraft.mods.enderutilities.init.EnderUtilitiesItems;
-import fi.dy.masa.minecraft.mods.enderutilities.reference.Reference;
 import net.minecraft.client.renderer.texture.IIconRegister;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.enchantment.Enchantment;
-import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.projectile.EntityArrow;
-import net.minecraft.init.Items;
-import net.minecraft.item.EnumAction;
 import net.minecraft.item.ItemBow;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.player.ArrowLooseEvent;
-import net.minecraftforge.event.entity.player.ArrowNockEvent;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import fi.dy.masa.minecraft.mods.enderutilities.creativetab.CreativeTab;
+import fi.dy.masa.minecraft.mods.enderutilities.entity.EntityEnderArrow;
+import fi.dy.masa.minecraft.mods.enderutilities.init.EnderUtilitiesItems;
+import fi.dy.masa.minecraft.mods.enderutilities.reference.Reference;
 
 public class EnderBow extends ItemBow
 {
@@ -41,7 +33,7 @@ public class EnderBow extends ItemBow
 	public void onPlayerStoppedUsing(ItemStack bowStack, World world, EntityPlayer player, int itemInUseCount)
 	{
 		int j = this.getMaxItemUseDuration(bowStack) - itemInUseCount;
-
+/*
 		ArrowLooseEvent event = new ArrowLooseEvent(player, bowStack, j);
 		MinecraftForge.EVENT_BUS.post(event);
 		if (event.isCanceled())
@@ -49,10 +41,10 @@ public class EnderBow extends ItemBow
 			return;
 		}
 		j = event.charge;
-
 		boolean flag = player.capabilities.isCreativeMode || EnchantmentHelper.getEnchantmentLevel(Enchantment.infinity.effectId, bowStack) > 0;
-
-		if (flag || player.inventory.hasItem(EnderUtilitiesItems.enderArrow))
+*/
+		// flag ||
+		if (player.inventory.hasItem(EnderUtilitiesItems.enderArrow))
 		{
 			float f = (float)j / 20.0F;
 			f = (f * f + f * 2.0F) / 3.0F;
@@ -67,35 +59,28 @@ public class EnderBow extends ItemBow
 				f = 1.0F;
 			}
 
-			EntityArrow entityarrow = new EntityArrow(world, player, f * 2.0F);
-
+			EntityEnderArrow entityenderarrow = new EntityEnderArrow(world, player, f * 2.0F);
+/*
 			if (f == 1.0F)
 			{
 				entityarrow.setIsCritical(true);
 			}
-
 			int k = EnchantmentHelper.getEnchantmentLevel(Enchantment.power.effectId, bowStack);
-
 			if (k > 0)
 			{
 					entityarrow.setDamage(entityarrow.getDamage() + (double)k * 0.5D + 0.5D);
 			}
-
 			int l = EnchantmentHelper.getEnchantmentLevel(Enchantment.punch.effectId, bowStack);
-
 			if (l > 0)
 			{
 				entityarrow.setKnockbackStrength(l);
 			}
-
 			if (EnchantmentHelper.getEnchantmentLevel(Enchantment.flame.effectId, bowStack) > 0)
 			{
 				entityarrow.setFire(100);
 			}
-
 			bowStack.damageItem(1, player);
 			world.playSoundAtEntity(player, "random.bow", 1.0F, 1.0F / (itemRand.nextFloat() * 0.4F + 1.2F) + f * 0.5F);
-
 			if (flag)
 			{
 				entityarrow.canBePickedUp = 2;
@@ -104,10 +89,13 @@ public class EnderBow extends ItemBow
 			{
 				//player.inventory.consumeInventoryItem(EnderUtilitiesItems.enderArrow);
 			}
-
+*/
+			player.inventory.consumeInventoryItem(EnderUtilitiesItems.enderArrow);
+			bowStack.damageItem(1, player);
+			world.playSoundAtEntity(player, "random.bow", 1.0F, 1.0F / (itemRand.nextFloat() * 0.4F + 1.2F) + f * 0.5F);
 			if (!world.isRemote)
 			{
-				world.spawnEntityInWorld(entityarrow);
+				world.spawnEntityInWorld(entityenderarrow);
 			}
 		}
 	}
@@ -118,13 +106,14 @@ public class EnderBow extends ItemBow
 	 */
 	public ItemStack onItemRightClick(ItemStack stack, World par2World, EntityPlayer player)
 	{
+/*
 		ArrowNockEvent event = new ArrowNockEvent(player, stack);
 		MinecraftForge.EVENT_BUS.post(event);
 		if (event.isCanceled())
 		{
 			return event.result;
 		}
-
+*/
 		if (player.inventory.hasItem(EnderUtilitiesItems.enderArrow))
 		{
 			player.setItemInUse(stack, this.getMaxItemUseDuration(stack));
