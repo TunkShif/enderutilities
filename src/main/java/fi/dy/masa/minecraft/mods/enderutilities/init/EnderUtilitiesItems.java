40d210cce8acf00005b7254f9372adaf5dcd2189
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2014-05-19 00:15:26 +0300

    Auto-commit @ 2014-05-19_00:15:27

--- src/main/java/fi/dy/masa/minecraft/mods/enderutilities/init/EnderUtilitiesItems.java
+++ src/main/java/fi/dy/masa/minecraft/mods/enderutilities/init/EnderUtilitiesItems.java
@@ -1,5 +1,6 @@
 package fi.dy.masa.minecraft.mods.enderutilities.init;
 
+import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -29,15 +30,24 @@ public class EnderUtilitiesItems
 		GameRegistry.registerItem(enderBucket, Reference.NAME_ITEM_ENDER_BUCKET);
 		GameRegistry.registerItem(enderLasso, Reference.NAME_ITEM_ENDER_LASSO);
 		GameRegistry.registerItem(enderPearlReusable, Reference.NAME_ITEM_ENDER_PEARL_REUSABLE);
+
+		ItemStack arrow = new ItemStack(Items.arrow);
+		ItemStack bucket = new ItemStack(Items.bucket);
+		ItemStack diamond = new ItemStack(Items.diamond);
 		ItemStack eye = new ItemStack(Items.ender_eye);
-		ItemStack pearl = new ItemStack(Items.ender_pearl);
 		ItemStack gold = new ItemStack(Items.gold_ingot);
-		ItemStack powder = new ItemStack(Items.blaze_powder);
 		ItemStack leather = new ItemStack(Items.leather);
+		ItemStack pearl = new ItemStack(Items.ender_pearl);
+		ItemStack powder = new ItemStack(Items.blaze_powder);
+		ItemStack rsblock = new ItemStack(Blocks.redstone_block);
 		ItemStack string = new ItemStack(Items.string);
 		ItemStack stick = new ItemStack(Items.stick);
-		GameRegistry.addShapelessRecipe(new ItemStack(enderArrow), "EAG", 'E', eye, 'A', new ItemStack(Items.arrow), 'G', gold);
+
+		GameRegistry.addShapelessRecipe(new ItemStack(enderArrow), eye, arrow, gold);
 		GameRegistry.addRecipe(new ItemStack(enderBag), "BLB", "LEL", "BLB", 'B', powder, 'L', leather, 'E', eye);
 		GameRegistry.addRecipe(new ItemStack(enderBow), "SE ", "S T", "SE ", 'S', string, 'T', stick, 'E', eye);
+		GameRegistry.addRecipe(new ItemStack(enderBucket), "DED", "DBD", "DED", 'D', diamond, 'E', eye, 'B', bucket);
+		GameRegistry.addRecipe(new ItemStack(enderLasso), "DED", "EGE", "DSD", 'D', diamond, 'E', eye, 'G', gold, 'S', string);
+		GameRegistry.addRecipe(new ItemStack(enderPearlReusable), "PEP", "ERE", "PEP", 'P', pearl, 'E', eye, 'R', rsblock);
 	}
 }
